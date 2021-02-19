df6848f41482bccb54dccb5a43684edacd0ccb45
mehmet ali <bm.m.alicetin@gmail.com>
2021-02-19 22:32:37 +0300

    Delete restApiExamples/src/main/java/com/spring/web/rest directory

--- restApiExamples/src/main/java/com/spring/web/rest/daoimpl/DAO.java
+++
@@ -1,8 +0,0 @@
-package com.spring.web.rest.daoimpl;
-
-import java.util.Collection;
-
-public interface DAO<T> {
-
-    Collection<T> findAll();
-}
--- restApiExamples/src/main/java/com/spring/web/rest/procedure/SQLQUERIES.java
+++
@@ -1,5 +0,0 @@
-package com.spring.web.rest.procedure;
-
-public final class SQLQUERIES {
-    public  static final String SELECT_ALL_CUSTOMERS = "AllCustomers";
-}
